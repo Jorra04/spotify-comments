6a8175cdc0818c0edcc37994a2aa993a953e958b
Jorra <jorra04@my.yorku.ca>
2025-04-26 01:27:22 -0400

    feat: only allow back button when the user is not on the search page

--- src/components/SideTray/index.tsx
+++ src/components/SideTray/index.tsx
@@ -1,21 +1,34 @@
 "use client";
 import { CircleArrowLeft } from "lucide-react";
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 import styles from "./styles.module.css";
 import { SearchArea } from "../";
+import { useEffect, useState } from "react";
 
 export default function SideTray() {
   const router = useRouter();
+  const pathName = usePathname();
+
+  const [backButtonDisabled, setBackButtonDisabled] = useState(false);
 
   const handleBack = () => {
+    if (backButtonDisabled) return;
     router.back();
   };
+
+  useEffect(() => {
+    setBackButtonDisabled(pathName === "/search");
+  }, [pathName]);
 
   return (
     <div className={styles.sideTrayContainer}>
       <SearchArea>
         <div className={styles.buttonContainer}>
-          <CircleArrowLeft size={48} color="white" onClick={handleBack} />
+          <CircleArrowLeft
+            size={48}
+            color={backButtonDisabled ? "grey" : "white"}
+            onClick={handleBack}
+          />
         </div>
       </SearchArea>
     </div>
